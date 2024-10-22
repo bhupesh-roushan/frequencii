f16ad555841f871b55a6c4a50fda5f54f0549adf
bhupesh-roushan <roushan.bhupesh@gmail.com>
2024-10-22 15:16:46 +0530

    image file updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,11 +14,11 @@ import {
   CarouselItem,
 } from "../components/ui/carousel";
 
-import hero1 from "../assets/images/hero1.jpg";
-import hero2 from "../assets/images/hero2.jpg";
-import hero3 from "../assets/images/hero3.jpg";
-import hero4 from "../assets/images/hero4.jpg";
-import hero5 from "../assets/images/hero5.jpg";
+import hero1 from "../assets/images/hero1.webp";
+import hero2 from "../assets/images/hero2.webp";
+import hero3 from "../assets/images/hero3.webp";
+import hero4 from "../assets/images/hero4.webp";
+import hero5 from "../assets/images/hero5.webp";
 import InfoSection from "../components/featureCard/InfoSection";
 import "../App.css";
 import CategoryCard from "../components/categoryCard/CategoryCard";
